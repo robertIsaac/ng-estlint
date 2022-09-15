263745d5b804a969729947fd2ce2c3fb860067bd
Robert Isaac (c) <irobert@vmware.com>
2022-09-15 18:08:15 +0300

    add foo library

---
+++ projects/foo/src/lib/foo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-foo',
+  template: `
+    <p>
+      foo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FooComponent {
+
+  constructor() { }
+
+}
